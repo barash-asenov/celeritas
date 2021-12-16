ef80618be98ccca0506d536faae395c2b4d2fea5
Barash Asenov <klaibthe@gmail.com>
2021-12-16 22:36:47 +0100

    [add] Added support for jet templates

--- myapp/handlers/handlers.go
+++ myapp/handlers/handlers.go
@@ -10,8 +10,8 @@ type Handlers struct {
 	App *celeritas.Celeritas
 }
 
-func (h *Handlers) Home(w http.ResponseWriter, r *http.Request) {
-	err := h.App.Render.Page(w, r, "home", nil, nil)
+func (h *Handlers) Home(rw http.ResponseWriter, r *http.Request) {
+	err := h.App.Render.Page(rw, r, "home", nil, nil)
 	if err != nil {
 		h.App.ErrorLog.Println("error rendering:", err)
 	}
